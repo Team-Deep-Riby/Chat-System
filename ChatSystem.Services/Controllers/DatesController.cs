80dee29dbde2d04d411d5e67dd65199dbf0f9b5b
glavunski <xiii1994@abv.bg>
2015-04-14 00:02:47 +0300

    Added DatesController

---
+++ ChatSystem.Services/Controllers/DatesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace ChatSystem.Services.Controllers
+{
+    using ChatSystem.Data;
+    using ChatSystem.Models;
+    using Services.Models;
+    [Authorize]
+    [RoutePrefix("api/Dates")]
+    public class DatesController : ApiController
+    {
+        private readonly IChatSystemData data;
+
+        public DatesController()
+        {
+            this.data = new ChatSystemData(new ApplicationDbContext());
+        }
+
+        [HttpGet]
+        [Route("{groupId:int}")]
+        public IHttpActionResult GetDatesByGroup(int groupId)
+        {
+            var currentUserId = User.Identity.GetUserId();
+            List<DateTime> messageDates = 
+                               (from m in data.Messages.All()
+                               where m.SenderId == currentUserId && m.GroupId == groupId
+                               orderby m.DateTime
+                               select m.DateTime).ToList();
+
+            return Ok(messageDates);
+
+        }
+
+    }
+}
