197cd3d1e7d53fdb8f5bfbbf29ed9199adf6d291
TzvetanIG <cecoig@mail.bg>
2015-04-14 22:13:25 +0300

    Rename Services.Models to ServicesModels

---
+++ ChatSystem.Services/ServicesModels/FriendViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatSystem.Services.Models
+{
+    public class FriendViewModel
+    {
+        public string Name { get; set; }
+
+        public int GroupId { get; set; }
+
+        public int UnreceivedMessages { get; set; }
+    }
+}
---
+++ ChatSystem.Services/ServicesModels/GroupIdViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatSystem.Services.Models
+{
+    public class GroupIdViewModel
+    {
+        public int GroupId { get; set; }
+    }
+}
