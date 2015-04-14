fd302b6f68c40b17090b27f3a795e59d40a17fbe
TzvetanIG <cecoig@mail.bg>
2015-04-14 20:37:59 +0300

    fixed GroupFullModel.cs

--- ChatSystem.Services/Models/GroupFullModel.cs
+++ ChatSystem.Services/Models/GroupFullModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ChatSystem.Services.Models
+﻿namespace ChatSystem.Services.Models
 {
-    public class GroupViewModel
+    public class GroupFullModel
     {
         public string GroupName { get; set; }
+
+        public int GroupId { get; set; }
+
+        public int UnreceivedMessages { get; set; }
     }
 }
