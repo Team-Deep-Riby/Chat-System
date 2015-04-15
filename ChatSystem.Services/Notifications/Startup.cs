6d8bd30a6cfa7afc0a01836438ad16cff52d233a
Konstantin Yamaliev <kyamaliev@gmail.com>
2015-04-15 16:47:24 +0300

    Add UI to project

---
+++ ChatSystem.Services/Notifications/Startup.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Startup.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+using ChatSystem.Services.Notifications;
+
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace ChatSystem.Services.Notifications
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
